cc960d1cdc31ad94651a1a7f199bf811a4adb849
mihomatsui <mihomatsui.24@gmail.com>
2021-12-08 23:16:55 +0900

    単体テストコード3を作成

--- spec/greengrocer_spec.rb
+++ spec/greengrocer_spec.rb
@@ -23,4 +23,34 @@ RSpec.describe Greengrocer do
       end
     end
   end
+
+  # 単体テスト3 正常系(register_productメソッド)
+  describe ".register_product" do
+    let(:product_params) do
+      [
+        { name: "トマト", price: 100 },
+        { name: "きゅうり", price: 200 }
+      ]
+    end
+    let(:greengrocer) { Greengrocer.new(product_params) }
+    let(:products) { greengrocer.products }
+    let(:adding_product_params) do
+      [
+        { name: "ごぼう", price: 250 },
+        { name: "れんこん", price: 350 }
+      ]
+    end
+    it "@productsの要素の数が,「product_paramsとadding_product_paramsの要素の数の和」と等しいこと" do
+      greengrocer.register_product(adding_product_params)
+      expect(greengrocer.products.size).to eq 4
+    end
+    it "@productsの最後の要素の名前が,adding_product_paramsの最後の要素の名前と等しいこと" do
+      greengrocer.register_product(adding_product_params)
+      expect(greengrocer.products[-1].name).to eq "れんこん"
+    end
+    it "@productsの最後の要素の名前が,adding_product_paramsの最後の要素の金額と等しいこと" do
+      greengrocer.register_product(adding_product_params)
+      expect(greengrocer.products[-1].price).to eq 350
+    end
+  end
 end
